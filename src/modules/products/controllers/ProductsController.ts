f76b6248e54e7c5cbe2a13976687052fab501a1e
Luan Ramos <luan.ramos@kolaresti.com.br>
2022-02-28 17:09:37 -0300

    controller de produtos

---
+++ src/modules/products/controllers/ProductsController.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+import CreateProductService from '../services/CreateProductService';
+import DeleteProductService from '../services/DeleteProductService';
+import ListProductService from '../services/ListProductService';
+import ShowProductService from '../services/ShowProductService';
+import UpdateProductService from '../services/UpdateProductService';
+
+export default class ProductsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const products = await new ListProductService().execute();
+
+    return response.json(products);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const product = await new ShowProductService().execute({ id });
+
+    return response.json(product);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+
+    const product = await new CreateProductService().execute({
+      name,
+      price,
+      quantity,
+    });
+
+    return response.json(product);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+    const { id } = request.params;
+
+    const product = await new UpdateProductService().execute({
+      id,
+      name,
+      price,
+      quantity,
+    });
+
+    return response.json(product);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    await new DeleteProductService().execute({ id });
+
+    return response.json([]);
+  }
+}
