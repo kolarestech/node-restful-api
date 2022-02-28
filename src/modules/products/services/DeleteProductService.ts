1cabe1e0c1f8ecaa74310e93508d7f7114023a51
Luan Ramos <luan.ramos@kolaresti.com.br>
2022-02-28 16:43:18 -0300

    serviço de delete  de um produto

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,20 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+export default class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('product not found');
+    }
+
+    await productsRepository.remove(product);
+  }
+}
